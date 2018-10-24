cac3e7a0691c03ff9d57e396bc5e7bb89e34dd14
Leonardo Borges <leo.daee@gmail.com>
2018-10-24 00:28:41 -0300

    Refactor project, change the structure of folders and creating functions in main

--- wordcloud/main.py
+++ wordcloud/main.py
@@ -6,28 +6,41 @@ from nltk.corpus import stopwords
 from wordcloud import WordCloud
 from PIL import Image
 import numpy as np
+import matplotlib.pyplot as plt
 
-# DEFINITIONS
-BASE_PATH = os.path.dirname(os.path.abspath(__file__))
-DATA_PATH = os.path.join(BASE_PATH, 'data')
-MASK_PATH = os.path.join(BASE_PATH, 'masks')
+from settings import *
+
+
+def define_stopwords(language='english', adicional_stopwords=[]):
+    return set(stopwords.words(language) + list(punctuation) + adicional_stopwords)
+
+
+def define_wordcloud(stopwords, maskname='cloud.png'):
+    mask = np.array(Image.open(os.path.join(MASK_PATH, maskname)))
+    wc = WordCloud(background_color="white", mask=mask, max_words=300, stopwords=stopwords,
+                   relative_scaling=0.15, collocations=False)
+    return wc
+
+
+def generate_wordcloud(wc, visualize=True, save_to_file=False, filename='wordcloud.png'):
+    if visualize:
+        plt.imshow(wc, interpolation='bilinear')
+        plt.axis("off")
+        plt.figure()
+
+    if save_to_file:
+        wc.to_file(os.path.join(WORDCLOUD_PATH, filename))
+
 
 
 df = pd.read_csv(os.path.join(DATA_PATH, 'Donald-Tweets!.csv'))
 series_tweets = df['Tweet_Text']
-text_zaratustra = "Eu vo-lo digo: é preciso ter um caos dentro de si para dar à luz uma estrela cintilante. Eu vo-lo digo: tendes ainda um caos dentro de vós outros. Ai! Aproxima-se o tempo em que o homem já não dará a luz às estrelas; aproxima-se o tempo do mais desprezível dos homens, do que já se não pode desprezar a si mesmo. Olhai! Eu vos mostro o último homem. Que vem a ser isso de amor, de criação, de ardente desejo, de estrela? — pergunta o último homem, revirando os olhos. A terra tornar-se-á então mais pequena, e sobre ela andará aos pulos o último homem, que tudo apouca."
+text = series_tweets.str.cat(sep=' ').lower()
 
-df2 = pd.read_excel(os.path.join(DATA_PATH, 'Acelerometro - Programa Acelera - 16h.xlsx'))
-series_message = df2['Message']
+adicional_stopwords = ['http', 'https']
+stopwords = define_stopwords(adicional_stopwords=adicional_stopwords)
 
-stop_words = set(stopwords.words('portuguese') + list(punctuation) + ['https', 'http'])
-
-mask = np.array(Image.open(os.path.join(MASK_PATH, "cloud2.png")))
-wc = WordCloud(background_color="white", mask=mask, max_words=300, stopwords=stop_words,
-               relative_scaling=0.15, collocations=False)
-
-# text = series_tweets.str.cat(sep=' ')
-# text = text_zaratustra
-text = series_message.str.cat(sep=' ').lower()
+wc = define_wordcloud(stopwords)
 wc.generate(text)
-wc.to_file(os.path.join(BASE_PATH, "wordcloud.png"))
+
+generate_wordcloud(wc, visualize=True, save_to_file=False, filename='WC-DonaldTweets.png')
---
+++ wordcloud/settings.py
@@ -0,0 +1,8 @@
+import os
+
+
+# DEFINITIONS
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+DATA_PATH = os.path.join(BASE_PATH, 'assets\data')
+MASK_PATH = os.path.join(BASE_PATH, 'assets\masks')
+WORDCLOUD_PATH = os.path.join(BASE_PATH, 'assets\wordclouds' )
